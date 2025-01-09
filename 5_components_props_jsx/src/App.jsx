5608250ddc0e8bc36ceddc42761ae22af08c8ffd
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-01-09 13:52:40 +0100

    5. Creating And Reusing a Component

--- 5_components_props_jsx/src/App.jsx
+++ 5_components_props_jsx/src/App.jsx
@@ -1,7 +1,22 @@
 import './App.css';
 
 function App() {
-  return <h1>Hello React</h1>;
+  return (
+    <>
+      <h1>Hello React</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </>
+  );
 }
+
+const Pizza = () => (
+  <div>
+    <img src='../pizzas/spinaci.jpg' alt='pizza spinaci' />
+    <h2>Pizza</h2>
+    <p>Tomato, mozzarella, spinach</p>
+  </div>
+);
 
 export default App;
