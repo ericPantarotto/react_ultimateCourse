0c4acaff5bf01d75eb35e29cb5701f3f1ecd1409
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-01-09 14:14:05 +0100

    5. Creating More Components

--- 5_components_props_jsx/src/App.jsx
+++ 5_components_props_jsx/src/App.jsx
@@ -1,9 +1,24 @@
+// import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <>
-      <h1>Hello React</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza</h1>;
+}
+
+function Menu() {
+  return (
+    <>
+      <h2>Our menu</h2>;
       <Pizza />
       <Pizza />
       <Pizza />
@@ -11,12 +26,23 @@ function App() {
   );
 }
 
-const Pizza = () => (
-  <div>
-    <img src='../pizzas/spinaci.jpg' alt='pizza spinaci' />
-    <h2>Pizza</h2>
-    <p>Tomato, mozzarella, spinach</p>
-  </div>
-);
+function Footer() {
+  return (
+    <footer>
+      {new Date().toLocaleDateString()}: We&apos;re currently open!
+    </footer>
+  );
+  // return React.createElement('footer', null, 'We\'re currently open! ')
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src='../pizzas/spinaci.jpg' alt='pizza spinaci' />
+      <h2>Pizza</h2>
+      <p>Tomato, mozzarella, spinach</p>
+    </div>
+  );
+}
 
 export default App;
