0942660f0ab2ba58c9a4ee8054cbc65830ea0c48
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-01-13 22:04:25 +0100

    6. Rendering the Items List

--- 6_travel-list/src/App.jsx
+++ 6_travel-list/src/App.jsx
@@ -1,4 +1,10 @@
+import PropTypes from 'prop-types';
 import './App.css';
+
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: false },
+];
 
 function App() {
   return (
@@ -24,8 +30,30 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className='list'>List</div>;
+  return (
+    <div className='list'>
+      <ul>
+        {initialItems.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+Item.propTypes = {
+  item: PropTypes.object,
+};
 
 function Stats() {
   return (
