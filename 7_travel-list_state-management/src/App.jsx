5017c9c9f96ed33ffba1f589c778479f6ee1ad63
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-01-14 15:45:16 +0100

    7. Module init

---
+++ 7_travel-list_state-management/src/App.jsx
@@ -0,0 +1,105 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import './App.css';
+
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: false },
+  { id: 3, description: 'Charger', quantity: 1, packed: false },
+];
+
+function App() {
+  return (
+    <div className='app'>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🏝️ Far Away 🧳</h1>;
+}
+
+function Form() {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+  
+  /**
+   * @param {Event} event
+   */
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: uuidv4() };
+    console.log(newItem);
+
+    setDescription('');
+    setQuantity(1);
+  }
+
+  return (
+    <form className='add-form' onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type='text'
+        placeholder='Item...'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+Form.propTypes = {
+  item: PropTypes.object,
+};
+
+function PackingList() {
+  return (
+    <div className='list'>
+      <ul>
+        {initialItems.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+Item.propTypes = {
+  item: PropTypes.object,
+};
+
+function Stats() {
+  return (
+    <footer className='stats'>
+      💼 You have X items on your list, and you already packed Y (...%)
+    </footer>
+  );
+}
+export default App;
