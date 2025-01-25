91d2c99cead880374dcc6925a4bd1a62dc43ec0f
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-01-25 23:22:54 +0100

    14. Fetching Weather Data & Displaying the Weather

--- 14__first-class-component/src/index.js
+++ 14__first-class-component/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
 // import Counter from './Counter';
+import AppV1 from './App-v1';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppV1 />
+    {/* <App /> */}
     {/* <Counter /> */}
   </React.StrictMode>
 );
