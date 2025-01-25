f42081c5988d0b204ab4b3c8908a184fd75dd5a5
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-01-25 23:16:35 +0100

    14. Starting the "Classy Weather" App

--- 14__first-class-component/src/index.js
+++ 14__first-class-component/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
-import Counter from './Counter';
+import App from './App';
+// import Counter from './Counter';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Counter />
+    <App />
+    {/* <Counter /> */}
   </React.StrictMode>
 );
