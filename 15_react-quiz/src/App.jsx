84a99bfd0f5056554bf6c33608ce5a33e3d0ba90
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-01-28 12:33:43 +0100

    15. The React Quiz App set-up

---
+++ 15_react-quiz/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './components/Header';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div className='app'>
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
+}
+
+export default App;
