266b76c42967aad64f8abac06341ebe19b69021d
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-01-31 16:50:42 +0100

    17. Implementing Main Pages and Routes

--- 16_spa_worldwise/src/App.jsx
+++ 16_spa_worldwise/src/App.jsx
@@ -1,7 +1,21 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import './App.css';
+import Pricing from './pages/Pricing';
+import Product from './pages/Product';
+import Homepage from './pages/Homepage';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
-  return <div>WorldWise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='product' element={<Product />} />
+        <Route path='pricing' element={<Pricing />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
