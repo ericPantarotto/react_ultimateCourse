21ac6ddf2ae367696ac64e65f4e0f988fa168355
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-02-17 16:19:21 +0100

    23. Styling Buttons: Element States and Transitions

--- 23_tailwind-pizza/src/features/order/CreateOrder.jsx
+++ 23_tailwind-pizza/src/features/order/CreateOrder.jsx
@@ -29,7 +29,7 @@ function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
   const formErrors = useActionData();
-  
+
   const cart = fakeCart;
 
   return (
@@ -74,7 +74,10 @@ function CreateOrder() {
 
         <div>
           <input type='hidden' name='cart' value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
+          <button
+            disabled={isSubmitting}
+            className='focus: inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold tracking-wide text-stone-700 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed'
+          >
             {isSubmitting ? 'Placing order....' : 'Order now '}
           </button>
         </div>
