89fe97aaad5ee0c420c8688123aa710778e5ceba
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-02-18 12:49:48 +0100

    23. Styling the Cart

--- 23_tailwind-pizza/src/ui/components/Button.jsx
+++ 23_tailwind-pizza/src/ui/components/Button.jsx
@@ -7,6 +7,8 @@ function Button({ children, to, type = 'primary' }) {
   const styles = {
     primary: base + ' px-4 py-3 md:px-6 md:py-4',
     small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
+    secondary:
+      'focus: inline-block rounded-full bg-transparent  font-semibold tracking-wide text-stone-400 uppercase transition-colors duration-300 hover:bg-stone-300 focus:text-stone-800 focus:bg-stone-300 focus:ring focus:ring-stone-200 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed border-2 border-stone-300 px-4 py-2.5 md:px-6 md:py-3.5',
   };
 
   if (to) {
