021f17aee4939e62e13f16c5f68dc97834cb8968
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-02-24 14:06:02 +0100

    25. module set-up

---
+++ 25_styled-comp_oasis/src/features/check-in-out/CheckoutButton.jsx
@@ -0,0 +1,12 @@
+import Button from '../../ui/Button';
+
+// function CheckoutButton({ bookingId }) {
+function CheckoutButton() {
+  return (
+    <Button variation='primary' size='small'>
+      Check out
+    </Button>
+  );
+}
+
+export default CheckoutButton;
---
+++ 25_styled-comp_oasis/src/ui/ConfirmDelete.jsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import Button from "./Button";
+import Heading from "./Heading";
+
+const StyledConfirmDelete = styled.div`
+  width: 40rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+
+  & p {
+    color: var(--color-grey-500);
+    margin-bottom: 1.2rem;
+  }
+
+  & div {
+    display: flex;
+    justify-content: flex-end;
+    gap: 1.2rem;
+  }
+`;
+
+// function ConfirmDelete({ resourceName, onConfirm, disabled }) {
+function ConfirmDelete({ resourceName, disabled }) {
+  return (
+    <StyledConfirmDelete>
+      <Heading as="h3">Delete {resourceName}</Heading>
+      <p>
+        Are you sure you want to delete this {resourceName} permanently? This
+        action cannot be undone.
+      </p>
+
+      <div>
+        <Button variation="secondary" disabled={disabled}>
+          Cancel
+        </Button>
+        <Button variation="danger" disabled={disabled}>
+          Delete
+        </Button>
+      </div>
+    </StyledConfirmDelete>
+  );
+}
+ConfirmDelete.propTypes = {
+  resourceName: PropTypes.string.isRequired,
+  onConfirm: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+};
+
+export default ConfirmDelete;
