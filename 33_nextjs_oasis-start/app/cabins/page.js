0b89132ba09ae01e3919cb77b65877b0710314c2
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-03-12 19:41:58 +0100

    33. module set-up next.js starting to build Oasis app

---
+++ 33_nextjs_oasis-start/app/cabins/page.js
@@ -0,0 +1,21 @@
+import Counter from '../components/Counter';
+
+export default async function Page() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+  // console.log(data); NOTE: will be logged in the server terminal & browser's console since recent Next.js versions
+
+  return (
+    <div>
+      <h1>Cabins page</h1>
+
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+
+      <Counter users={data} />
+    </div>
+  );
+}
