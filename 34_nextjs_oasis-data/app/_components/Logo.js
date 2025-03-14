d344897ee1980a7f75670fde82d172e154f07b87
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-03-14 16:44:38 +0100

    34. next.js data fetching - module set-up

---
+++ 34_nextjs_oasis-data/app/_components/Logo.js
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import logo from '@/public/logo.png'
+
+function Logo() {
+  return (
+    <Link href='/' className='z-10 flex items-center gap-4'>
+      <Image src={logo} height='60' width='60' quality={100} alt='The Wild Oasis logo' />
+      
+      <span className='text-primary-100 text-xl font-semibold'>
+        The Wild Oasis
+      </span>
+    </Link>
+  );
+}
+
+export default Logo;
